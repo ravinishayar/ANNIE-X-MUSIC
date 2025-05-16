b318e6daebfb4b836617646a430a580683780f14
ravinishayar <remixoldhindi@gmail.com>
2025-05-16 11:56:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,12 +18,12 @@ BOT_NAME = getenv("BOT_NAME", "˹sɪʏᴀ ✘ 𝙼ᴜsɪᴄ˼ ♪")
 ASSUSERNAME = getenv("ASSUSERNAME", "Testioiiiiiibot")
 EVALOP = list(map(int, getenv("EVALOP", "6797202080").split()))
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-LOGGER_ID = int(getenv("LOGGER_ID", -1002014167331))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002600355202))
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 # External APIs - Get these from their respective providers
 GPT_API = getenv("GPT_API")
 DEEP_API = getenv("DEEP_API")
-OWNER_ID = int(getenv("OWNER_ID", 5147822244))
+OWNER_ID = int(getenv("OWNER_ID", 6308382997))
 
 # Heroku deployment settings - Refer to Heroku documentation on how to obtain these
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
